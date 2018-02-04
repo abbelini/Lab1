c3e187e4cf927675abd39a9dd578b8a5ed5f2cd7
abbelini <albert.k.bergstrom@hotmail.com>
2018-02-04 15:22:09 +0100

    asd

--- NN/src/source.py
+++ NN/src/source.py
@@ -7,8 +7,11 @@ def nonlin(x,deriv=False):
     return 1/(1+np.exp(-x))
 
 TEST_SIZE = 700
-LEARN_RATE = 0.01
-DECAY_RATE = 0.95
+LEARN_RATE = 0.005
+DECAY_RATE = 0.98
+NUM_HIDDENLAYER_1=5
+NUM_HIDDENLAYER_2=5
+
 #First, we get the data from the file
 print("Reading testdata")
 traininginput = list()
@@ -26,8 +29,8 @@ for line in file:
         
 testinput = traininginput[-700:] # Get the last 700
 testoutput = trainingoutput[-700:] # Get the last 700
-traininginput = traininginput[:-700]
-trainingoutput = trainingoutput[:-700]
+traininginput = traininginput[:-700] # Get the rest
+trainingoutput = trainingoutput[:-700] # Get the rest
 
 # Convert to array to allow for matrix functions
 traininginput = np.asarray(traininginput)
@@ -35,39 +38,68 @@ trainingoutput = np.asarray(trainingoutput)
 testinput = np.asarray(testinput)
 testoutput = np.asarray(testoutput)
 
-np.random.seed(1)
+np.random.seed()
 
 # randomly initialize our weights with mean 0
-syn0 = 2*np.random.random((3,10)) - 1
-syn1 = 2*np.random.random((10,10)) - 1
-syn2 = 2*np.random.random((10,1)) - 1
+syn0 = 2*np.random.random((3,NUM_HIDDENLAYER_1)) - 1
+#w0_1 = 2*np.random.random(NUM_HIDDENLAYER_1) - 1
 
-for j in range(160000):
+syn1 = 2*np.random.random((NUM_HIDDENLAYER_1,NUM_HIDDENLAYER_2)) - 1
+#w0_2 = 2*np.random.random(NUM_HIDDENLAYER_2) - 1
+syn2 = 2*np.random.random((NUM_HIDDENLAYER_2,1)) - 1
+#w0_3 = 2*np.random.random(1) - 1
+
+for j in range(120000):
 
 
 
-    # Feed forward through layers 0, 1, and 2
-    l0 = traininginput
-    l1 = nonlin(np.dot(l0,syn0))
-    l2 = nonlin(np.dot(l1,syn1))
-    l3 = nonlin(np.dot(l2,syn2))
+    # Feed forward through layers 0, 1, 2 and 3
+    Input_Layer = traininginput
+    Hidden_Layer_1 = nonlin((np.dot(Input_Layer,syn0)))#w0_1
+    Hidden_Layer_2 = nonlin((np.dot(Hidden_Layer_1,syn1)))#w0_2
+    Output_Layer = nonlin((np.dot(Hidden_Layer_2,syn2)))#w0_3
     
 
-    l3_error = trainingoutput - l3
+
+    l3_error = trainingoutput - Output_Layer
+    l3_delta = l3_error*nonlin(Output_Layer,deriv=True)
+    #w0_3_delta = l3_error*LEARN_RATE
     
     if (j% 10000) == 0:
         print ("Error:" + str(np.mean(np.abs(l3_error))))
         
-
-    l3_delta = l3_error*nonlin(l3,deriv=True)*LEARN_RATE
-
     l2_error = l3_delta.dot(syn2.T)
-    l2_delta = l2_error * nonlin(l2,deriv=True)*LEARN_RATE
-
+    l2_delta = l2_error * nonlin(Hidden_Layer_2,deriv=True)
+    #w0_2_delta = l2_error*LEARN_RATE
+    
     l1_error = l2_delta.dot(syn1.T)
-    l1_delta = l1_error * nonlin(l1,deriv=True)*LEARN_RATE
+    l1_delta = l1_error * nonlin(Hidden_Layer_1,deriv=True)
+    #w0_1_delta = l1_error*LEARN_RATE
+    #w0_3 += np.sum(w0_3_delta, axis=0)
+    syn2 += (Hidden_Layer_2.T.dot(l3_delta)*LEARN_RATE)
+    #w0_2 += np.sum(w0_2_delta, axis=0)
+    syn1 += (Hidden_Layer_1.T.dot(l2_delta)*LEARN_RATE)
+    #w0_1 += np.sum(w0_1_delta, axis=0)
+    syn0 += (Input_Layer.T.dot(l1_delta)*LEARN_RATE)
+print("Done,Starting test")
 
-    syn2 += (l2.T.dot(l3_delta))
-    syn1 += (l1.T.dot(l2_delta))
-    syn0 += (l0.T.dot(l1_delta))
-print("Done")
+rightanswers=0
+wronganswers=0
+Input_Layer = testinput
+Hidden_Layer_1 = nonlin((np.dot(Input_Layer,syn0)))#w0_1
+Hidden_Layer_2 = nonlin((np.dot(Hidden_Layer_1,syn1)))#w0_2
+Output_Layer = nonlin((np.dot(Hidden_Layer_2,syn2)))#w0_3
+
+for j in range(700):
+    if Output_Layer[j]>=0.5 and testoutput[j] == 0.75:
+        rightanswers = rightanswers + 1 #Right
+    elif Output_Layer[j]<0.5 and testoutput[j] == 0.25:
+        rightanswers = rightanswers + 1 #Right
+    else:
+        wronganswers = wronganswers + 1 #Wrong
+testinput = np.asarray(testinput)
+testoutput = np.asarray(testoutput)
+print("Right")
+print(rightanswers)
+print("Wrong")
+print(wronganswers)
